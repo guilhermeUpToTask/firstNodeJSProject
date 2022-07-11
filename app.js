83853d168ad745d13b211b8687c90adc1771d5c4
guilherme <guilherme256teretelteltel@gmail.com>
2022-07-11 13:18:48 -0300

    nodemon dependencies added

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+const { request } = require("express");
 const express = require("express"); //import express
 
 const app = express(); // iniatilizate express like a function
+
+app.listen(4002, () => console.log("server is running in port 4002"));
+
+app.get("/first_route", (request, response) => { // get the route /frist_route
+    return response.json({
+        message : "first route acessed"
+    }); // send the response to that route
+});
