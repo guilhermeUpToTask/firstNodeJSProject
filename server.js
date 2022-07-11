1c49ec6f4d85b954051c99a809f398c3c6175e5e
guilherme <guilherme256teretelteltel@gmail.com>
2022-07-11 12:27:35 -0300

    implemented simples routes of urls in server.js

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
+const { resolveNaptr } = require('dns');
 const http = require('http'); // import the htt library
 
 http.createServer((request, response) =>{ // create serve with a resquet and a response
     response.writeHead(200, { 'Content-Type': 'application/json' }); // send a response with a type and the type of the content
-    response.end(JSON.stringify({
-      message: "Minha primeira aplicação Node.js"
-    }));
+    
+
+    if (request.url === "/produtos"){
+      response.end(JSON.stringify({
+        message: "rota dos produtos"
+      }));
+    }
+    if (request.url === "/usuario"){
+      response.end(JSON.stringify({
+        message : "rota do usuario"
+      }));
+    }
+    
 }).listen(4001, () => console.log('serve running in port 4001')); // when the port being listen recive something will be redirect to here
